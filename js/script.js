2e53e841ddc39944d3df98222a15f02d0f10587b
Miguel Herrera <38386750+DeathAngel94@users.noreply.github.com>
2018-08-09 02:32:07 -0500

    Code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-
+(funtion(d){
+ let tabs = Array.prototype.slice.apply(d.querySelectorAll('.tabs_item'));
+ let panels = Array.prototype.slice.apply(d.querySelectorAll('.panels_item));
+  d.getElementById("tabs").addEventListener('Click', e => {
+   if(e.target.ClassList.contains('tabs_item')){
+     let i = tabs.indexOf(e.target);
+     tabs.map(tab => tab.ClassList.remove('active'));
+     tabs[i].ClassList.add('active');
+     panels.map(panel => panel.ClassList.remove('active'));
+     panels[i].ClassList.add('active');
+     }
+   }
+}(document));
